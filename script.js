0b9f5af1ec36e4dfb5d08f4b33e510a12a6de4d1
Elisabeth1690 <43107055+Elisabeth1690@users.noreply.github.com>
2026-06-25 22:00:19 +0200

    Remove a message from the array
    Removed an item from the array of messages.

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ const quotes = [
     "Du er råkul",
     "Et sted skinner solen like mye som smilet ditt",
     "Det er innsiden som teller",
-    "Salat dagen derpå er godt for sjel og sinn",
     
 ];
 
